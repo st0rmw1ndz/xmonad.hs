5f157759c33a6b4b941e041865c9a0da6bdfd7c3
frosty <passedgoandgot200@gmail.com>
2024-07-27 22:57:10 -0400

    update xmobar modules and add some new ones

--- xmobar-bottom.hs
+++ xmobar-bottom.hs
@@ -8,15 +8,15 @@ config =
       bgColor = "#121212",
       fgColor = "#cccccc",
       position = Bottom,
-      template = " %disk-root%  %disk-home% <fc=#777777>|</fc> %local-ip% <fc=#777777>|</fc> %ssid% }{ %now-playing% <fc=#777777>|</fc> %volume% ",
+      template = " %disk-root% <fc=#777777>|</fc> %disk-home% <fc=#777777>|</fc> %status-redshift% <fc=#777777>|</fc> %status-udiskie% }{ %now-playing% <fc=#777777>|</fc> %volume% ",
       commands =
         [ Run XMonadLog,
-          Run $ Com "monitors" ["local_ip"] "local-ip" 6000,
-          Run $ Com "monitors" ["ssid"] "ssid" 6000,
-          Run $ Com "monitors" ["disk", "/"] "disk-root" 6000,
-          Run $ Com "monitors" ["disk", "/home"] "disk-home" 6000,
-          Run $ PipeReader "/tmp/pipe-volume" "volume",
-          Run $ PipeReader "/tmp/pipe-now-playing" "now-playing"
+          Run $ Com "monitors" ["disk", "/"] "disk-root" 35000,
+          Run $ Com "monitors" ["disk", "/home"] "disk-home" 35000,
+          Run $ Com "monitors" ["service", "redshift"] "status-redshift" 6000,
+          Run $ Com "monitors" ["service", "udiskie"] "status-udiskie" 6000,
+          Run $ PipeReader "/tmp/pipe-now-playing" "now-playing",
+          Run $ PipeReader "/tmp/pipe-volume" "volume"
         ]
     }
 
--- xmobar-top.hs
+++ xmobar-top.hs
@@ -8,15 +8,14 @@ config =
       bgColor = "#121212",
       fgColor = "#cccccc",
       position = Top,
-      template = " %XMonadLog% }{ \57958 %cpu% <fc=#777777>|</fc> \61381 %memory% <fc=#777777>|</fc> %load% <fc=#777777>|</fc> %battery% <fc=#777777>|</fc> <fc=#f7e83b>%date%</fc> ",
+      template = " %XMonadLog% }{ \61381 %memory% <fc=#777777>|</fc> %load% <fc=#777777>|</fc> \62153 %cpu% <fc=#777777>|</fc> %battery% <fc=#777777>|</fc> <fc=#f7e83b>%date%</fc> ",
       commands =
         [ Run XMonadLog,
-          Run $ PipeReader "/tmp/pipe-volume" "volume",
-          Run $ Com "monitors" ["battery"] "battery" 600,
-          Run $ Com "monitors" ["cpu"] "cpu" 50,
-          Run $ Com "monitors" ["memory"] "memory" 50,
+          Run $ Com "monitors" ["battery"] "battery" 1200,
+          Run $ Com "monitors" ["cpu"] "cpu" 100,
+          Run $ Com "monitors" ["memory"] "memory" 100,
           Run $ Com "monitors" ["load"] "load" 100,
-          Run $ Date "%m/%d/%Y %H:%M:%S" "date" 10
+          Run $ Date "%m/%d/%Y %-I:%M %p" "date" 600
         ]
     }
 
