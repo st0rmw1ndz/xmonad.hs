70edf56a13b71bdc5b099d510118596506d22ebf
frosty <passedgoandgot200@gmail.com>
2024-07-29 04:01:09 -0400

    remove references to top/bottom bars and switch to single

--- xmobar.hs
+++ xmobar.hs
@@ -8,15 +8,14 @@ config =
       bgColor = "#121212",
       fgColor = "#cccccc",
       position = Top,
-      template = " %XMonadLog% }{ %volume% <fc=#777777>|</fc> %load% <fc=#777777>|</fc> %battery% <fc=#777777>|</fc> <fc=#f7e83b>%date%</fc> ",
+      template = " %XMonadLog% }{ %weather% <fc=#777777>|</fc> %volume% <fc=#777777>|</fc> %load% <fc=#777777>|</fc> %battery% <fc=#777777>|</fc> <fc=#f7e83b>%date%</fc> ",
       commands =
         [ Run XMonadLog,
-          Run $ Com "monitors" ["battery"] "battery" 1200,
-          Run $ Com "monitors" ["cpu"] "cpu" 100,
-          Run $ Com "monitors" ["memory"] "memory" 100,
-          Run $ Com "monitors" ["load"] "load" 100,
           Run $ PipeReader "/tmp/pipe-volume" "volume",
-          Run $ Date "%m/%d/%Y %-I:%M %p" "date" 600
+          Run $ Com "monitors" ["load"] "load" 100,
+          Run $ PipeReader "/tmp/pipe-weather" "weather",
+          Run $ Com "monitors" ["battery"] "battery" 1200,
+          Run $ Date "%m/%d/%Y %H:%M" "date" 600
         ]
     }
 
